70cebc36d9fb354f1d21e582461a7fddd7e7b691
Edgar Ivan Mancillas Garcia <emancillas04@gmail.com>
2022-04-29 18:28:05 -0500

    Iconos agregados

--- src/components/dash/dashboard.jsx
+++ src/components/dash/dashboard.jsx
@@ -1,18 +1,25 @@
 import './App.css';
-import { SideBar } from "./components/sidebar/sidebar";
+//import { SideBar } from "./components/sidebar/sidebar";
 import { HeadBar } from "./components/headbar/headbar";
 import { SideBarCollapse } from './components/sidebar-collapse/sidebar-collapse';
+import { CategoryBox } from './components/category-box/category-box';
+import Dashboard from './components/dash/dash';
 
 
 function App() {
+    const [open, setOpen] = React.useState(true);
+    const toggleDrawer = () => {
+    setOpen(!open);
   return (
     <div className="App">
       <HeadBar/>
       <div className='App__container'>
-        <SideBar/>
+        <Dashboard/>
       </div>
     </div>
   );
 }
 
 export default App;
+
+
--- src/components/headbar/headbar.js
+++ src/components/headbar/headbar.js
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./headbar.css";
 import "../sidebar/sidebar";
 import menu from '../../assets/icons/negro/menu.png';
 import logo from '../../assets/logo.png';
+import AccountMenu from "../UserProfile/userProfile";
+import searcheezy from "../../assets/icons/negro/search.png";
+import notifyeezy from "../../assets/icons/negro/notify.png";
+import lightmode from "../../assets/icons/negro/lightmode.png"
+
 
 
 function HeadBar() {
@@ -13,6 +18,19 @@ function HeadBar() {
             </div>
             <div className="headbar__element-logo">
                 <img src={logo} alt="logo"/>
+            </div>
+            <div className="header__search">
+                <img src={searcheezy} />
+            </div>
+            <div className="header__light">
+                <img src={lightmode} />
+            </div>
+            <div className="header__notify">
+                <img src={notifyeezy} />
+            </div>
+
+            <div className="header__user">
+                <AccountMenu/>
             </div>
         </div>
     );
